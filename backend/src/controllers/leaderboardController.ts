10da8760a48f7a836b3a31b0326f096f85671b67
PURAM DINESH <dinesh.05hk@gmail.com>
2026-04-04 00:51:37 +0530

    Change import path for prisma to include .js extension

--- backend/src/controllers/leaderboardController.ts
+++ backend/src/controllers/leaderboardController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import prisma from '../config/db';
+import prisma from '../config/db.js';
 import { getCache, setCache, clearCacheByPrefix } from '../services/cacheService';
 
 const LEADERBOARD_TTL = 120; // Cache leaderboard for 2 minutes
