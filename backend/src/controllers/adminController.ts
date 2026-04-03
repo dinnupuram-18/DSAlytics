38839510416e8b65bc22de90c68935404fdfecc9
PURAM DINESH <dinesh.05hk@gmail.com>
2026-04-04 00:43:51 +0530

    Change import path for prisma to use .js extension

--- backend/src/controllers/adminController.ts
+++ backend/src/controllers/adminController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import prisma from '../config/db';
+import prisma from '../config/db.js';
 
 export const addBatch = async (req: any, res: Response): Promise<void> => {
     try {
